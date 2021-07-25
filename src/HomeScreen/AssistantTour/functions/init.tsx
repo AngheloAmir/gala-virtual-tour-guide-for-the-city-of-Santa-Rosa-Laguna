b6e2ae7989b5269128ed17035a02d6df223ff52c
Anghelo Amir <angheloamir@gmail.com>
2021-07-25 14:49:35 +0800

    0.4.1.1 Added GPS accurancy warning

--- src/HomeScreen/AssistantTour/functions/init.tsx
+++ src/HomeScreen/AssistantTour/functions/init.tsx
@@ -2,12 +2,11 @@
     Define what will happen after the map is loaded and draw
     -- used in AssistanTourIndex.tsx
 */
-
 import * as Location from 'expo-location';
 import { setDialogMessage, permissionLocationNotGranted, setMapWasLoaded,
          setUserPosition, setMapCenter, setZoomlevel, setMapLock } from '../localstateAPI/actions';
 import { IntroPosition } from '../../../../database/assistantour/tours';
-import { getLocation } from './getLocation';
+import { GPSRANGE } from '../../../../database/assistantour/tours';
 
 export async function init( localDispatch :any) {
     try {
@@ -19,6 +18,13 @@ export async function init( localDispatch :any) {
             localDispatch( setZoomlevel(17) );
             localDispatch( setMapWasLoaded() );
             localDispatch( setMapLock(false) );
+            //@ts-ignore
+            if(userlocation.accurancy >= 100)
+                localDispatch( setDialogMessage('GPS Acccurancy',
+                'Caution! Your GPS accrancy is too low! You are withing ' + userlocation.accurancy + 'meter. ' +
+                'To improve your GPS accurancy, enable location in your device and connect it to a pocket wifi. ' +
+                'The accurancy is affected by weather condition and obsructions such as trees. ' +
+                'Visit https://www.gps.gov/systems/gps/performance/accuracy/ to learn more.') );
         }, 3000);
     }
     catch(err) {
@@ -40,4 +46,16 @@ async function RequestPermission() {
     const { status } = await Location.requestForegroundPermissionsAsync();
     if (status == 'granted') return true;
     throw new Error('PermissionException');
+}
+
+async function getLocation() {
+    const location = await Location.getCurrentPositionAsync({});
+    if( location.coords.latitude  > GPSRANGE.y || location.coords.latitude < GPSRANGE.endy ||
+        location.coords.longitude < GPSRANGE.x || location.coords.longitude > GPSRANGE.endx )
+        throw new Error('OutOfRangeException');
+    return {
+        lat: location.coords.latitude,
+        lng: location.coords.longitude,
+        accurancy: location.coords.accuracy,
+    };
 }
